8abbce7cea5b7531f1f2813b4f69721e7541cb51
Hardik Singh Behl <hardik.behl7444@gmail.com>
2021-05-21 13:07:07 +0530

    initial project commit

---
+++ src/main/java/com/hardik/plutocracy/PersonalFinanceManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.hardik.plutocracy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalFinanceManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalFinanceManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hardik/plutocracy/PersonalFinanceManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hardik.plutocracy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalFinanceManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
